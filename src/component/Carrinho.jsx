2d5433514a9ed43007228bbf659e6e355eef07bc
MOACIR DA SILVA NETO <moacir_silva-neto@estudante.sesisenai.org.br>
2023-07-10 21:28:59 -0300

    atulizando carrinho

--- src/component/Carrinho.jsx
+++ src/component/Carrinho.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const Carrinho = ({ cartItems, cartTotal }) => {
-  const total = typeof cartTotal === 'number' ? cartTotal.toFixed(2) : '0.00';
+  const calcularTotal = () => {
+    let total = 0;
+    cartItems.forEach((item) => {
+      total += item.jogo.preco * item.quantidade;
+    });
+    return total.toFixed(2);
+  };
 
   return (
     <div>
@@ -13,7 +19,7 @@ const Carrinho = ({ cartItems, cartTotal }) => {
               <li key={item.jogo.game_id}>{item.jogo.nome}</li>
             ))}
           </ul>
-          <p>Valor Total: R${total}</p>
+          <p>Valor Total: R${calcularTotal()}</p>
         </div>
       ) : (
         <p>O carrinho está vazio.</p>
